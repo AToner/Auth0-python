d6f49095267cd09b89ee1675647083495879546d
Andrew Toner <git@andytoner.com>
2018-05-08 09:20:06 -0700

    Bringing up to date

--- test.sh
+++ test.sh
@@ -1,7 +1,15 @@
-curl --request GET --url http://localhost:8080/ping
-curl --request GET --url http://localhost:8080/secure
-TOKEN=$(curl --request GET --url 'http://localhost:8080/gettoken?username=auth0-user@andytoner.com&password=atestuser')
+URL=http://python-flask-dev.us-east-1.elasticbeanstalk.com
+#URL=http://localhost:8080
+curl --request GET --url $URL/ping
+echo
+sleep 1
+curl --request GET --url $URL/secure
+echo
+sleep 1
+TOKEN=$(curl --request GET --url $URL'/gettoken?username=auth0-user@andytoner.com&password=atestuser')
+echo $TOKEN
+sleep 1
 curl --header 'authorization: Bearer '$TOKEN \
        --request GET \
-       --url http://localhost:8080/secure
-
+       --url $URL/secure
+echo
